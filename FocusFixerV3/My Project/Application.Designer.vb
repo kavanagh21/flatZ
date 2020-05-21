200c28c67c0c37e297ddae636c119d48b69d968f
kavanagh21 <d.kavanagh@bham.ac.uk>
2020-05-21 22:56:39 +0100

    Change assembly name

--- FocusFixerV3/My Project/Application.Designer.vb
+++ FocusFixerV3/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.flatZ.Form1
+            Me.MainForm = Global.flatZ.mainForm
         End Sub
     End Class
 End Namespace
